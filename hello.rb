17a041dc39359cc264d8592f7df9dee64ee3e56e
Matthias Hryniszak <padcom@gmail.com>
2011-05-06 19:48:44 +0200

    Made the app do something :D

--- hello.rb
+++ hello.rb
@@ -1,8 +1,27 @@
+require 'rubygems'
 require 'sinatra'
 require "sinatra/reloader" if development?
 require 'erb'
+require 'dm-core'
+require 'dm-migrations'
+
+DataMapper.setup(:default, ENV["DATABASE_URL"] || "sqlite3://#{Dir.pwd}/development.db")
+
+class Post
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :title, String
+end
+
+DataMapper.auto_upgrade!
 
 get '/' do
-  @message = "Hello, world! from sinatra"
+  @posts = Post.all
   erb :index
+end
+
+post '/new' do
+  Post.create(:title => params[:title]) 
+  redirect to("/")
 end
