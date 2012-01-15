0b8f9b0203a2d8bb48e79c63ac85018e499c3e19
Matthias Hryniszak <padcom@gmail.com>
2012-01-15 19:36:39 +0100

    Code cleanup

--- hello.rb
+++ hello.rb
@@ -4,21 +4,38 @@ require "sinatra/reloader" if development?
 require 'erb'
 require 'dm-timestamps'
 require 'mongo'
+require 'uri'
+
+class Database
+	def self.initialize
+		uri   = ENV["MONGOLAB_URI"] || "mongodb://localhost:27017/ihatejsf"
+		mongo = Mongo::Connection.from_uri(uri)
+		uri   = URI.parse(uri)
+		@@db   = mongo.db(uri.path.gsub(/^\//, ''))
+	end
+
+	def self.db
+		@@db
+	end
+
+	def self.[](collection)
+		@@db.collection(collection)
+	end
+
+	def self.next_id(collection)
+	    self["counters"].find_and_modify({ :query => { :_id => collection }, :update => { "$inc" => { :sequence => 1 } } })
+	end
+end
+
+Database.initialize
 
 class Logger
 	def self.initialize
-		uri = URI.parse(ENV["MONGOLAB_URI"] || "mongodb://localhost:27017/ihatejsf")
-		db = Mongo::Connection.new(uri.host, uri.port).db(uri.path[1..-1])
-		if not uri.user.nil?
-			db.authenticate(uri.user, uri.password)
-		end
-		if db.collection_names.include?("log")
-			@@collection = db.collection("log")
+		if !Database.db.collection_names.include?("log")
+			@@collection = Database.db.create_collection("log", :capped => true, :size => 100000, :max => 1000)
 		else
-			@@collection = db.create_collection("log", :capped => true, :size => 100000, :max => 1000)
+			@@collection = Database["log"]
 		end
-		@@posts = db.collection("posts")
-		@@counters = db.collection("counters")
 	end
 
 	def self.log(s)
@@ -27,28 +44,20 @@ class Logger
 			:message   => s
 		})
 	end
-	
-	def self.posts
-	    @@posts
-	end
-	
-	def self.next_id
-	    @@counters.find_and_modify({ :query => { :_id => "posts" }, :update => { "$inc" => { :sequence => 1 } } })
-	end
 end
 
 Logger.initialize
 
 get '/' do
   Logger.log("GET /")
-  @posts = Logger.posts.find.sort([[ 'created', 'descending' ]])
+  @posts = Database["posts"].find.sort([[ 'created', 'descending' ]])
   erb :index
 end
 
 post '/complain' do
   Logger.log("POST /complain")
-  Logger.posts.insert({
-    :_id => Logger.next_id,
+  Database["posts"].insert({
+    :_id => Database.next_id("posts"),
     :author => params[:nick],
     :text => params[:text],
     :created => Time.new
@@ -58,18 +67,18 @@ end
 
 get '/list' do
   Logger.log("GET /list")
-  @posts = Logger.posts.find.sort([[ 'created', 'descending' ]])
+  @posts = Database["posts"].find.sort([[ 'created', 'descending' ]])
   erb :posts, :layout => false
 end
 
 get '/post/:id' do
   Logger.log("GET /post/" + params[:id])
-  post = Logger.posts.find_one({ :_id => Integer(params[:id]) })
+  post = Database["posts"].find_one({ :_id => Integer(params[:id]) })
   if post
     erb :single_post, :locals => { :post => post }
   else
     Logger.log("post with id " + params[:id] + " not found - returning index")
-    @posts = Logger.posts.find.sort([[ 'created', 'descending' ]])
+    @posts = Database["posts"].find.sort([[ 'created', 'descending' ]])
     erb :index
   end
 end
