c7d3d21942acb1bc388351755bf92f946ebb79f8
Matthias Hryniszak <padcom@gmail.com>
2014-06-16 09:21:03 +0200

    New version based on AngularJS and Bootstrap

--- hello.rb
+++ hello.rb
@@ -5,6 +5,7 @@ require 'erb'
 require 'dm-timestamps'
 require 'mongo'
 require 'uri'
+require 'json'
 
 set :erubis, :escape_html => true
 
@@ -62,6 +63,7 @@ get '/' do
   erb :index
 end
 
+
 post '/complain' do
   Logger.log("POST /complain")
   Database["posts"].insert({
@@ -70,6 +72,15 @@ post '/complain' do
     :text => params[:text],
     :created => Time.new
   })
+  "OK"
+end
+
+post '/complain2' do
+  Logger.log("POST /complain")
+  post = JSON.parse(request.body.read.to_s)
+  post[:_id] = Database.next_id("posts")["sequence"]
+  post[:created] = Time.new
+  Database["posts"].insert(post)
   "OK"
 end
 
@@ -89,4 +100,14 @@ get '/post/:id' do
     @posts = Database["posts"].find.sort([[ 'created', 'descending' ]])
     erb :index
   end
+end
+
+get '/ng' do
+  Logger.log("GET /ng");
+  erb :ng, :layout => false
+end
+
+get '/api/posts' do
+  content_type :json
+  Database["posts"].find.sort([[ 'created', 'descending' ]]).to_a.to_json
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,29 +1,75 @@
-$(document).ready(function() {
-    $(".created_at").timeago();
-    $("#add-post").dialog({
-        autoOpen: false,
-        title: "JSF Sucks 4 me because ....",
-        width: '400',
-        height: '270',
-        buttons: {
-            "Cancel": function() {
-                $("#post-clear").click();
-                $(this).dialog("close");
-            },
-            "Share with the class": function() {
-                $("#posts").html("<div class='wait-panel'></div>");
-                $.post("/complain", $("#post").serialize(), function(data) {
-                    $("#post-clear").click();
-                    $("#posts").load("/list", function() {
-                        $(".created_at").timeago();
-                    });
-                    $('#add-post').dialog("close");
-                });
-            }
+(function() {
+  var posts = [];
+
+  var app = angular.module('ihatejsf', ['ihatejsf-directives', 'ihatejsf-filters', 'ui.bootstrap']);
+
+  app.controller('PostsController', function(PostsService) {
+    var controller = this;
+    this.posts = [];
+    PostsService.getAllPosts().success(function(data) {
+      controller.posts = data;
+      posts = data;
+    });
+  });
+
+  app.controller('ComplaintController', function(PostsService, $modal, $scope) {
+    $scope.open = function() {
+      var modalInstance = $modal.open({
+        templateUrl: 'add-complaint.html',
+        controller: function($scope, $modalInstance, $log) {
+          $scope.data = { author: "Anonymous", text: "" };
+
+          $scope.ok = function() {
+            $modalInstance.close({ data: $scope, author: $scope.data.author, text: $scope.data.text });
+          };
+
+          $scope.cancel = function() {
+            $modalInstance.dismiss('cancel');
+          };
         }
-    });
-    $("#link-to-post").click(function(e) {
-        e.preventDefault();
-        $("#add-post").dialog("open");
-    });
-});
+      });
+
+      modalInstance.result.then(function(complaint) {
+        PostsService.addPost(complaint);
+      });
+    };
+  });
+
+  app.service('PostsService', function($http) {
+    this.getAllPosts = function() {
+      return $http.get("/api/posts");
+    };
+    this.addPost = function(post) {
+      post.created = new Date();
+      posts.unshift(post);
+      console.log(post);
+      $http.post("/complain2", { nick: post.author, text: post.text }).success(function(response) { console.log(response); });
+    };
+  });
+})();
+
+(function() {
+  var app = angular.module('ihatejsf-directives', []);
+  app.directive('post', function() {
+    return {
+      restrict: 'E',
+      scope: {
+        post: '='
+      },
+      templateUrl: '_post.html',
+      controller: function($scope, $sce) {
+        var facebookLikeLink = "http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fihatejsf.com%2Fpost/" + $scope.post._id + "&amp;send=true&amp;layout=button_count&amp;width=250&amp;show_faces=true&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21";
+        $scope.post.facebookLikeLink = $sce.trustAsResourceUrl(facebookLikeLink);
+      }
+    };
+  });
+})();
+
+(function() {
+  var app = angular.module('ihatejsf-filters', []);
+  app.filter('since', function() {
+    return function(date) {
+      return moment(date).fromNow();
+    }
+  });
+})();
